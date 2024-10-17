82203c623e85b3b8b92b601da60c95ff163a511a
anonys6 <shakibalam998@gmail.com>
2024-10-17 23:19:09 +0530

    adding microsoft clarity

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ import styles from "@/styles/Home.module.css";
 import "./globals.css";
 import Footer from "@/components/Footer";
 import TextInput from "@/components/TextInput";
+import Script from 'next/script';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,6 +23,17 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en" data-theme="light">
+            <head>
+                <Script id="clarity-script" strategy="afterInteractive">
+                    {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "${process.env.NEXT_PUBLIC_CLARITY_PROJECT_ID}");
+          `}
+                </Script>
+            </head>
             <body className={inter.className}>
                 <TextProvider>
                     <Navbar />
