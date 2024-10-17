da27e110f3fba56d4441bfef22f7ca58df4293ad
anonys6 <shakibalam998@gmail.com>
2024-10-17 23:42:28 +0530

    adding meta tags for seo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,27 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
     title: "Case Converter",
     description: "Convert between camelCase, kebab-case, PascalCase, and snake_case.",
+    openGraph: {
+        title: "Live Case Converter: Convert Your Text Cases Online for FREE",
+        description: "This free online case converter comes with features like case conversion, super-easy text editing and 10+ formatting functionalities. Try now.",
+        url: "https://www.livecaseconverter.com/",
+        siteName: "Live Case Converter",
+        images: [
+            {
+                url: "https://www.livecaseconverter.com/og-image.svg",
+                width: 1200,
+                height: 630,
+            },
+        ],
+        locale: "en_US",
+        type: "website",
+    },
+    other: {
+        "robots": "index, follow",
+        "keywords": "case converter, online case converter, free case converter, advanced case converter",
+    },
 };
+
 
 export default function RootLayout({
     children,
@@ -23,18 +43,36 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en" data-theme="light">
-            <head>
+            <body className={inter.className}>
+                {/* Metadata script */}
+                <Script type="application/ld+json" id="json-ld">
+                    {`
+                        {
+                          "@context": "http://schema.org",
+                          "@type": "WebSite",
+                          "name": "Live Case Converter",
+                          "url": "https://www.livecaseconverter.com/",
+                          "description": "This free online case converter comes with features like case conversion, super-easy text editing and 10+ formatting functionalities.",
+                          "author": {
+                              "@type": "Person",
+                              "name": "Md Shakib Alam"
+                          }
+                        }
+                    `}
+                </Script>
+
+                {/* Clarity script */}
                 <Script id="clarity-script" strategy="afterInteractive">
                     {`
-            (function(c,l,a,r,i,t,y){
-                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-            })(window, document, "clarity", "script", "${process.env.NEXT_PUBLIC_CLARITY_PROJECT_ID}");
-          `}
+                        (function(c,l,a,r,i,t,y){
+                            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+                        })(window, document, "clarity", "script", "${process.env.NEXT_PUBLIC_CLARITY_PROJECT_ID}");
+                    `}
                 </Script>
-            </head>
-            <body className={inter.className}>
+
+                {/* App Components */}
                 <TextProvider>
                     <Navbar />
                     <div className={styles.container}>
